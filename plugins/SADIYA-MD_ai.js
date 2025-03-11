efd3356dbdae4f6101fde9e5da43f5f65a849cbe
cybetAsh <dinethnimsara281@gmail.com>
2025-03-11 21:06:59 +0530

    Add files via upload

---
+++ plugins/SADIYA-MD_ai.js
@@ -0,0 +1,23 @@
+/**
+══════════════════════════════════════════════════════════════════════════════════════════════════════
+
+░██████╗░█████╗░██████╗░██╗██╗░░░██╗░█████╗░░░░░░███╗░░░███╗██████╗░ 
+██╔════╝██╔══██╗██╔══██╗██║╚██╗░██╔╝██╔══██╗░░░░░████╗░████║██╔══██╗ 
+╚█████╗░███████║██║░░██║██║░╚████╔╝░███████║░░░░░██╔████╔██║██║░░██║ 
+░╚═══██╗██╔══██║██║░░██║██║░░╚██╔╝░░██╔══██║░░░░░██║╚██╔╝██║██║░░██║ 
+██████╔╝██║░░██║██████╔╝██║░░░██║░░░██║░░██║░░░░░██║░╚═╝░██║██████╔╝ 
+╚═════╝░╚═╝░░╚═╝╚═════╝░╚═╝░░░╚═╝░░░╚═╝░░╚═╝░░░░░╚═╝░░░░░╚═╝╚═════╝░ 
+
+══════════════════════════════════════════════════════════════════════════════════════════════════════
+*
+   * @project_name : SADIYA-MD
+   * @author : Sadiya Tech
+   * @youtube : https://www.youtube.com/@Sadiya-Tech
+   * @description : SADIYA-MD ,Javascript WhatsApp Bot Made By Sadiya Tech.
+*
+* 
+   * Created By Sadiya Tech.
+   * © 2025 SADIYA-MD .
+*/
+
+const _0x3230cc=_0x13f7;(function(_0x80de89,_0x155b01){const _0x40b47e=_0x13f7,_0x46150e=_0x80de89();while(!![]){try{const _0x531f6f=parseInt(_0x40b47e(0x75))/0x1*(parseInt(_0x40b47e(0x66))/0x2)+parseInt(_0x40b47e(0x72))/0x3+parseInt(_0x40b47e(0x74))/0x4+parseInt(_0x40b47e(0x76))/0x5*(-parseInt(_0x40b47e(0x6f))/0x6)+parseInt(_0x40b47e(0x69))/0x7*(-parseInt(_0x40b47e(0x64))/0x8)+parseInt(_0x40b47e(0x70))/0x9+-parseInt(_0x40b47e(0x6a))/0xa;if(_0x531f6f===_0x155b01)break;else _0x46150e['push'](_0x46150e['shift']());}catch(_0x11fb00){_0x46150e['push'](_0x46150e['shift']());}}}(_0x33b2,0xcb3d7));function _0x33b2(){const _0x4d956c=['❌\x20*I\x20Couldn\x27t\x20find\x20anything.\x20Please\x20try\x20again\x20later...*','321696TBohrt','@s.whatsapp.net','❗\x20*Error\x20Info:*\x20','1132901fpZrxI','727370XbMyzs','Using\x20Artificial\x20Interligant.','log','https://apis.davidcyriltech.my.id/blackbox?q=','../lib/functions','1281996vUbjqP','6361632fsVDLg','sendMessage','1342707vbKlAm','../lib/command','2869860khLwQh','2yHhdXY','15xPEnNJ','32LENYXr'];_0x33b2=function(){return _0x4d956c;};return _0x33b2();}function _0x13f7(_0x1cc75e,_0x21a708){const _0x33b224=_0x33b2();return _0x13f7=function(_0x13f743,_0x122969){_0x13f743=_0x13f743-0x64;let _0x4c1302=_0x33b224[_0x13f743];return _0x4c1302;},_0x13f7(_0x1cc75e,_0x21a708);}const {cmd,commands}=require(_0x3230cc(0x73)),{fetchJson}=require(_0x3230cc(0x6e));cmd({'pattern':'ai','react':'🤖','desc':_0x3230cc(0x6b),'category':'ai','filename':__filename},async(_0x2352cf,_0x339c40,_0xcd3fbd,{from:_0x3aee14,quoted:_0x68c634,body:_0xea961a,isCmd:_0x11d968,command:_0x4caea7,args:_0x4abb80,q:_0x5a89ce,isGroup:_0x14959a,sender:_0x509f65,senderNumber:_0x3f1ec6,botNumber2:_0x5cfb0a,botNumber:_0x2cb52d,pushname:_0x420f25,isMe:_0x53e035,isOwner:_0x37c073,groupMetadata:_0x3d3164,groupName:_0x4e4587,participants:_0x4206b3,groupAdmins:_0x5cabf7,isBotAdmins:_0x50be3b,isAdmins:_0x15a5d9,reply:_0x50d599})=>{const _0x4e840e=_0x3230cc;try{const _0x204e36=await fetchJson(_0x4e840e(0x6d)+_0x5a89ce);await _0x2352cf[_0x4e840e(0x71)](_0x3aee14,{'text':_0x204e36['response']},{'quoted':_0x339c40});}catch(_0xd5e2fb){console[_0x4e840e(0x6c)](_0xd5e2fb),_0x50d599(_0x4e840e(0x65)),await _0x2352cf[_0x4e840e(0x71)](_0x2cb52d+_0x4e840e(0x67),{'text':_0x4e840e(0x68)+_0xd5e2fb},{'quoted':_0x339c40});}});
